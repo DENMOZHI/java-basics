09f97e424ef46aec7d68bbe45edce4568088aaa0
densri5gh <dhanushyappm@gmail.com>
2018-05-29 22:08:57 +0530

    commit test

---
+++ src/org/ksr/javaprogram/DataTypeExample.java
@@ -0,0 +1,19 @@
+package org.ksr.javaprogram;
+
+public class DataTypeExample {
+
+	
+	
+	public static void main(String[] args) {
+		
+		
+		int i=10;
+		
+		
+		
+		for (int j = 0; j < i; j++) {
+			System.out.println("J value is"+j);	
+		}
+		
+	}
+}

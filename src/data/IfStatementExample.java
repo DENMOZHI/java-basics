ba72396a2dae9be8837c6e20fe09248d65a4b112
densri5gh <dhanushyappm@gmail.com>
2018-05-30 15:00:25 +0530

    Box

---
+++ src/data/IfStatementExample.java
@@ -0,0 +1,13 @@
+package data;
+
+public class IfStatementExample{
+	public static void  main(String[]args)
+		
+	{int marks=65;
+		if(marks<100)
+		{
+		System.out.println("number is less then 100");	
+	}
+	}
+
+}

9b7b2edc5f01356fb1b51881eb607b7ead86dc5e
densri5gh <dhanushyappm@gmail.com>
2018-06-08 10:11:39 +0530

    arguments

---
+++ src/number/Sum.java
@@ -0,0 +1,9 @@
+package number;
+public class Sum { 
+public static void main(String[] args) {	
+double  T1 = Double . parseDouble(args [0]);
+double  T2 = Double. parseDouble(args [1]);
+double total=T1+T2;
+System.out.println(total);
+}
+}

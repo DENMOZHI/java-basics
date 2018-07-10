9ef7be41b1a9b332c8488435ad6e4da9d7d01307
densri5gh <dhanushyappm@gmail.com>
2018-07-10 18:44:05 +0530

    split charAt

---
+++ src/length/Show.java
@@ -0,0 +1,27 @@
+package length;
+
+public class Show {
+	public static void main(String []args)
+	{
+		String s="Hello";
+		String Lowercase=s.toLowerCase(); 
+		for(int i=0,j=s.length()-1;i<=j;i++,j--)
+			if(i!=j)
+	
+		{
+				byte[] bytes =charAt(i).getBytes();
+			//System.out.println(Lowercase.charAt(i));
+		//	System.out.println(Lowercase.charAt(j));
+		}
+			else
+			{
+				//`System.out.println(Lowercase.charAt(i));
+			}
+	}
+
+	private static String charAt(int i) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	}
